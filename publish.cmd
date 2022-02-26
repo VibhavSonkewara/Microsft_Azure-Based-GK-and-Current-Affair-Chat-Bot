f935094ef56f80193183a779970047f9bcad9778
IV2604 <85401407+IV2604@users.noreply.github.com>
2022-02-27 02:59:46 +0530

    Bhumika's Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotknowledge-bot-bff7-Web-Deploy.pubxml -p:Password=TCh0fPhaqmrM13twMarXyk6nLBqNZEj8icPTpsWab6YXMwgiyfGmfJuggcgR
+
